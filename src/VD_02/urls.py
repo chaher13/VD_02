069dc0f62b7b4c44193d134e1e60a1368531488d
chaher13 <chahercrbs@gmail.com>
2023-11-03 02:47:30 +0100

    changement for production

--- src/VD_02/urls.py
+++ src/VD_02/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from account.views import register_user, index, login_user, user_dashboard, logout_user
 from contact.views import contact
 
@@ -29,3 +31,8 @@ urlpatterns = [
     path('logout/', logout_user, name='logout'),
     path('contact/', contact, name='contact'),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT)
